ed932b845792693561656d6530486d183c53698c
111031075jack <111031075@live.asia.edu.tw>
2026-02-07 12:05:02 +0800

    update

--- src/day3/StringDemo1.java
+++ src/day3/StringDemo1.java
@@ -5,14 +5,19 @@ public class StringDemo1 {
 	public static void main(String[] args) {
 		String s1 = new String("Java");
 		String s2 = new String("Java");
-		String s3 = new String("jAVa");
-		String s4 = "Java";
+		String s3 = new String("jAvA");
 		
+		System.out.printf("s1 = %s%n", s1);
+		System.out.printf("s2 = %s%n", s2);
+		System.out.printf("s3 = %s%n", s3);
 		System.out.printf("s1 == s2 %b%n", s1 == s2);
-		System.out.printf("s1 == s3 %b%n", s1 == s3);
-		System.out.printf("s1 == s4 %b%n", s1 == s4);
+		System.out.printf("s1.equals(s2) %b%n", s1.equals(s2));
+		System.out.printf("s1.equals(s3) %b%n", s1.equals(s3));
+		// 不分大小寫比較
+		System.out.printf("s1.equalsIgnoreCase(s3) %b%n", s1.equalsIgnoreCase(s3));
 		
-		System.out.println(s1.equals(s4));
+		
+
 	}
-	
+
 }
