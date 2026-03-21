3cc8aa63d33bb5250891b1ebc2939e018dce4e95
111031075jack <111031075@live.asia.edu.tw>
2026-03-21 12:00:23 +0800

    update

---
+++ src/day7/case1/Cup.java
@@ -0,0 +1,27 @@
+package day7.case1;
+
+/**
+ * 強迫規定杯子的容量
+ * 定義杯子一定要能做的事
+ * 
+ *           +-----+
+ *           | Cup |  <interface>
+ *           +-----+
+ *            ^   ^
+ *            |   |   implements
+ * +----------+   +-----+
+ * | PaperCup |   | Mug |  <class>
+ * +----------+   +-----+
+ * */
+
+public interface Cup {
+	// 介面屬性一定會加入 public static final
+	// 若沒寫 java 會在編譯時期自動補上, 所以不寫也可以
+	// public static final int capacity = 250; // cc
+	int capacity = 250; // java 會在編譯時期自動補上public static final
+	
+	void make(); // 製作方式 => public abstract void make();
+	void fill(); // 裝水    => public abstract void fill();
+	
+	
+}
